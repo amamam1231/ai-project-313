10e3161d30d458e7adbea918e79566606192133e
amamam1231 <securevpntg@gmail.com>
2026-02-16 00:37:16 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        marker: ['Permanent Marker', 'cursive'],
+        comic: ['Comic Neue', 'cursive'],
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+        'float': 'float 6s ease-in-out infinite',
+        'shake': 'shake 0.5s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '75%': { transform: 'translateX(5px)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
